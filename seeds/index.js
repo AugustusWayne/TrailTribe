d152eac81a1f4b8dc5902351ce42643b5feefa5b
AugustusWayne <ishaan.sharma0510@gmail.com>
2024-07-27 20:12:34 +0530

    Added Maps using Mapbox

--- seeds/index.js
+++ seeds/index.js
@@ -28,12 +28,14 @@ const seedDB = async () => {
         const random = Math.floor(Math.random() * 100) + 1;
         const price = Math.floor(Math.random() * 100) + 1
         const camp = new Campground({
+            //MY USER ID
             author: '66a0f4e09ec182a313528da4',
             title: `${sample(descriptors)} ${sample(Places)}`,
             location: `${cities[random].city}, ${cities[random].admin_name}`,
             //image: `https://picsum.photos/400?random=${Math.random()}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti nostrum quibusdam eius atque accusantium in dignissimos. Iusto, hic enim sequi quae consequuntur harum aut nobis labore nemo provident. Fuga, similique?',
             price,
+            geometry: { type: 'Point', coordinates: [ 72.975662, 19.187882 ] },
             images: [
                 {
                     url: 'https://res.cloudinary.com/dtczgrboc/image/upload/v1722022747/TrailTribe/cg8l3yfczhrk6ukkcss3.png',
